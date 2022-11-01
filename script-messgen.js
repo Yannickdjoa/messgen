e266706e8b77d580b9196c1bd281208e8f1a1344
yannw83 <yannickdjoa2@gmail.com>
2022-11-01 09:14:37 +0100

    initial code

---
+++ script-messgen.js
@@ -0,0 +1,3 @@
+const userInput = "";
+const horoscopeList = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const month = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
